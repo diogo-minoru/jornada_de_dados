6565f7bffbd499439bc2251a9f04605757b713cd
Diogo Minoru <diogominoru@hotmail.com>
2025-01-22 20:31:27 -0300

    adicionando aula1

---
+++ 2-python/aula1/README.md
@@ -0,0 +1,2 @@
+# Aula 01 Bootcamp Python - Jornada de Dados
+
---
+++ 2-python/aula1/aula1.py
@@ -0,0 +1,11 @@
+# Exercício 01
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+# print(len(input("Digite o seu nome: ")))
+
+# Exercício 02
+
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+
+print(a + b)
