fa3f852b40ab566e0a3778b41f699014fedac874
Diogo Minoru <diogominoru@hotmail.com>
2025-01-29 20:43:03 -0300

    solicionando desafio aula 2

--- 2-python/aula2/desafio.py
+++ 2-python/aula2/desafio.py
@@ -1,16 +1,38 @@
 ### Desafio - Refatorar o projeto da aula 1 evitando Bugs!
 
-# 1) Solicita ao usuário que digite seu nome
-nome = input("Digite o seu nome: ")
+# 1) Solicita ao usuário o nome
+def nome_usuario():
+    while True:
+        try:
+            nome = input("Digite o seu nome: ")
+            print(f"Olá {nome}.")
+            solicitar_salario()
+            break
+        except ValueError:
+            print("Não são permitidos números")
 
 # 2) Solicita ao usuário que digite o valor do seu salário
-salario = float(input("Digite o seu salário atual: "))
+def solicitar_salario():
+    while True:
+        try:
+            salario = float(input("Informe o seu salário atual: "))
+            solicitar_bonus()
+            break
+        except ValueError:
+            print("Apenas números são permitidos.")
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
-bonus = float(input("Digite o valor do bonus recebido: "))
+def solicitar_bonus():
+    while True:
+        try:
+            bonus = float(input("Digite o valor do bonus recebido: "))
+            calculo_bonus_final()
+        except ValueError:
+            print("Apenas números são permitidos.")
 
 # 4) Calcule o valor do bônus final
-bonus_final = salario * bonus
+def calculo_bonus_final():
+    bonus_final = salario * bonus
 
 # 5) Imprima cálculo do KPI para o usuário
 print(f"Salario de {salario} + {bonus} = {bonus_final}")
