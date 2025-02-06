92efe0428ae3ac58368469659c6e11e77f42f150
Diogo Minoru <diogominoru@hotmail.com>
2025-02-06 20:25:06 -0300

    finalizando exercicios aula 3

--- 2-python/aula3/aula3.py
+++ 2-python/aula3/aula3.py
@@ -110,36 +110,106 @@ def retornar_pares(lista):
 
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
-
-vendas = {
-    {"Item": "Queijo", "Categoria": "Laticínios", "Quantidade": 4},
-    {"Item": "Banana", "Categoria": "Hortifruti", "Quantidade": 1},
-    {"Item": "Frango", "Categoria": "Carnes", "Quantidade": 3},
-    {"Item": "Refrigerante", "Categoria": "Bebidas", "Quantidade": 4},
-    {"Item": "Cenoura", "Categoria": "Hortifruti", "Quantidade": 3},
-    {"Item": "Água", "Categoria": "Bebidas", "Quantidade": 1},
-    {"Item": "Sabão em pó", "Categoria": "Limpeza", "Quantidade": 4},
-    {"Item": "Peixe", "Categoria": "Carnes", "Quantidade": 1},
-    {"Item": "Alface", "Categoria": "Hortifruti", "Quantidade": 3},
-    {"Item": "Manteiga", "Categoria": "Laticínios", "Quantidade": 4}
+"""
+compras = {
+    1: {"Item": "Queijo", "Categoria": "Laticínios", "Quantidade": 4},
+    2: {"Item": "Banana", "Categoria": "Hortifruti", "Quantidade": 1},
+    3: {"Item": "Frango", "Categoria": "Carnes", "Quantidade": 3},
+    4: {"Item": "Refrigerante", "Categoria": "Bebidas", "Quantidade": 4},
+    5: {"Item": "Cenoura", "Categoria": "Hortifruti", "Quantidade": 3},
+    6: {"Item": "Água", "Categoria": "Bebidas", "Quantidade": 1},
+    7: {"Item": "Sabão em pó", "Categoria": "Limpeza", "Quantidade": 4},
+    8: {"Item": "Peixe", "Categoria": "Carnes", "Quantidade": 1},
+    9: {"Item": "Alface", "Categoria": "Hortifruti", "Quantidade": 3},
+    10: {"Item": "Manteiga", "Categoria": "Laticínios", "Quantidade": 4}
 }
 
-for i in vendas:
-    print(i)
+soma_categorias = {}
 
+for i in compras.values():
+    categoria = i["Categoria"]
+    quantidade = i["Quantidade"]
+    soma_categorias[categoria] = soma_categorias.get(categoria, 0) + quantidade
+
+print(soma_categorias)
+"""
 ### Exercícios com WHILE
 
 ### Exercício 11. Leitura de Dados até Flag
 # Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
 
+def funcao_sair():
+    while True:
+        entrada = input("Digite 'Sair' para sair do loop.: ")
+        if entrada.lower() == "sair":
+            print("Você saiu do loop!!")
+            break
+        else:
+            pass
+
 ### Exercício 12. Validação de Entrada
 # Solicitar ao usuário um número dentro de um intervalo específico até que a entrada seja válida.
 
+def numero_intervalo():
+    while True:
+        entrada = float(input("Digite um número entre 1 e 100: "))
+        if 1 <= entrada <= 100:
+            print(f"Você digitou o número {entrada}, portanto sairá do loop.")
+            break
+        else:
+            print("Você digitou um número fora do intervalo, digite novamente.")
+            
 ### Exercício 13. Consumo de API Simulado
 # Simular o consumo de uma API paginada, onde cada "página" de dados é processada em loop até que não haja mais páginas.
+
+def consumir_paginas(paginas):
+    while True:
+        for pagina in range(paginas + 1):
+            entrada = input("Deseja ir para a próxima página? (s/n): ")
+            if entrada.lower() == "s" and pagina <= paginas + 1:
+                print(f"Você está na página {pagina}")
+                print(f"Você irá para a página {pagina + 1}")
+            elif entrada.lower() == "n":
+                print("Você cancelou a operação.")
+                break
+            else:
+                print("Você já consumiu todas as páginas.")
+                break
+            pagina += 1
 
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um limite máximo de tentativas.
 
+import random
+
+def conexao_servidor():
+    numero_tentativas = 1
+    while numero_tentativas <= 10:
+        numero_aleatorio = random.randint(1, 20)
+        if numero_aleatorio == 10:
+            print("Conexão realizada com sucesso!")
+            break
+        else:
+            print("Conexão falhou, tentando novamente.")
+            print(f"Número de tentativas {numero_tentativas}")
+            if numero_tentativas == 10:
+                print("Tentativas máximas alcançadas, tente novamente mais tarde.")
+        
+        numero_tentativas += 1
+
 ### Exercício 15. Processamento de Dados com Condição de Parada
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
+
+import random
+
+numeros_aleatorios = [random.randint(1, 100) for _ in range(20)]
+
+def encontrar_numero(numero):
+    for i in numeros_aleatorios:
+        if numero not in numeros_aleatorios:
+            print(numeros_aleatorios)
+            print(f"O número desejado {numero} não está na lista.")
+            break
+        elif i == numero:
+            print(numeros_aleatorios)
+            print(f"Você encontrou o número desejado {numero} na posição {numeros_aleatorios.index(i)}")
