d1eb74ce5d30f580ae986ef83c8cd3b518d285ed
Diogo Minoru <diogominoru@hotmail.com>
2025-02-10 19:20:01 -0300

    finalizando desafio aula 2

--- 2-python/aula2/desafio.py
+++ 2-python/aula2/desafio.py
@@ -3,44 +3,41 @@
 # 1) Solicita ao usuário o nome
 def nome_usuario():
     while True:
-        try:
-            nome = input("Digite o seu nome: ")
+        nome = input("Digite o seu nome: ")
+        if nome.replace(" ", "").isalpha():
             print(f"Olá {nome}.")
-            solicitar_salario()
             break
-        except ValueError:
+        else:
             print("Não são permitidos números")
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 def solicitar_salario():
     while True:
-        try:
-            salario = float(input("Informe o seu salário atual: "))
-            solicitar_bonus()
-            break
-        except ValueError:
-            print("Apenas números são permitidos.")
+        salario = input("Informe o seu salário atual: ")
+        if salario.isdigit():
+            print(f"Salário informado: {salario}")
+            return float(salario)
+        else:
+            print("Apenas números são permitidos para o salário.")
+        
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 def solicitar_bonus():
     while True:
-        try:
-            bonus = float(input("Digite o valor do bonus recebido: "))
-            calculo_bonus_final()
-        except ValueError:
-            print("Apenas números são permitidos.")
+        bonus = input("Digite o valor do bonus recebido (%): ")
+        if bonus.isdigit():
+            print(f"Bônus informado: {bonus}")
+            return float(bonus)
+        else:
+            print("Apenas números são permitidos para o bônus.")
+        
 
 # 4) Calcule o valor do bônus final
 def calculo_bonus_final():
-    bonus_final = salario * bonus
+    nome = nome_usuario()
+    salario = solicitar_salario()
+    bonus = solicitar_bonus()
+    bonus_final = salario * (bonus/100)
+    print(f"{nome} considerando o salário de R${salario} e o bônus de {bonus} %, o seu bônus final será de R$ {bonus_final}, resultando um total de R$ {salario + bonus_final}")
 
-# 5) Imprima cálculo do KPI para o usuário
-print(f"Salario de {salario} + {bonus} = {bonus_final}")
-
-# 6) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
-print(f"{nome} seu salario final será de {bonus_final}")
-
-# Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
-# Digitar texto no valor do salário
-# Digitar porcentagem utilizando vírgula e não ponto
-# Não digitar nenhum valor de salário ou bônus
+calculo_bonus_final()
