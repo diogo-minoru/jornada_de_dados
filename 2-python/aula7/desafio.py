5e005fb9d290cc37bb73be3a0cba0d9e5df67686
Diogo Minoru <diogominoru@hotmail.com>
2025-03-10 19:27:01 -0300

    adicionando desafio aula 7

--- 2-python/aula7/desafio.py
+++ 2-python/aula7/desafio.py
@@ -1,1 +1,41 @@
-#Análise de Vendas de Produtos Objetivo: Dado um arquivo CSV contendo dados de vendas de produtos, o desafio consiste em ler os dados, processá-los em um dicionário para análise e, por fim, calcular e reportar as vendas totais por categoria de produto.
+# Análise de Vendas de Produtos Objetivo: Dado um arquivo CSV contendo dados de vendas de produtos, 
+# o desafio consiste em ler os dados, processá-los em um dicionário para análise e, por fim, calcular 
+# e reportar as vendas totais por categoria de produto.
+
+import csv
+
+path = "vendas.csv"
+arquivo_dict = []
+
+def ler_csv(nome_arquivo):
+    with open(nome_arquivo, mode='r', encoding='utf-8') as arquivo:
+        leitor = csv.DictReader(arquivo)
+        return list(leitor)
+
+
+def processar_dados(dados):
+    categorias = {}
+    for item in dados:
+        categoria = item['Categoria']
+        if categoria not in categorias:
+            categorias[categoria] = []
+        categorias[categoria].append(item)
+    return categorias
+
+def calcular_vendas_categoria(dados):
+    vendas_por_categoria = {}
+    for categoria, itens in dados.items():
+        total_vendas = sum(int(item['Quantidade']) * int(item['Venda']) for item in itens)
+        vendas_por_categoria[categoria] = total_vendas
+    return vendas_por_categoria
+
+def main():
+    nome_arquivo = 'vendas.csv'
+    dados_brutos = ler_csv(nome_arquivo)
+    dados_processados = processar_dados(dados_brutos)
+    vendas_categoria = calcular_vendas_categoria(dados_processados)
+    for categoria, total in vendas_categoria.items():
+        print(f'{categoria}: ${total}')
+
+if __name__ == '__main__':
+    main()
