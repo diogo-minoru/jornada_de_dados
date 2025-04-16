35ead4bbb21edfa95707be3018b07e821539c9a2
diogominoru <diogominoru@hotmail.com>
2025-04-16 20:01:06 -0300

    aula 18

--- 2-python/aula18/aula18.py
+++ 2-python/aula18/aula18.py
@@ -5,3 +5,13 @@ URL = "https://pokeapi.co/api/v2/pokemon/15"
 response = requests.get(URL)
 data = response.json()
 print(data.keys())
+
+data_types = data["types"]
+
+types_list = []
+
+for type in data_types:
+    types_list.append(type["type"]["name"])
+
+types = f"{data["name"]} " + ', '.join(types_list)
+print(types)
